813518c17b5c63c8a4b33abc01b0e0704b4a6948
Veikko Laiho <veikko.laiho@student.laurea.fi>
2016-02-18 11:53:48 +0200

    Basic stuffs

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<div id="header">
+			<h2>Hieno sivu hermanni :-D</h2>
+			<div class="logout"><a href="index.php">Log out</a></div>
+		</div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
-echo 'kurwa';
-?>
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Serverside project</title>
+		<meta charset=utf-8>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+		<body>
+		<?php include("header.php"); ?>
+			<div id="container">
+				<p><label class="label" for="username">Username:</label><input id="username" type="text" name="username" size="30" maxlength="30" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>"></p>
+				<p><label class="label" for="password">Password:</label><input id="password" type="password" name="password" size="30" maxlength="15" value="" ></p>
+				<p><input id="login" type="submit" name="login" value="Log in">
+				<a href="register.php">Create account</a></p>
+			</div>
+		</body>
+</html>
