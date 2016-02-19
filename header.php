3efba98fcb7fd33279c1d00c3e20b11d9869011b
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-02-19 19:54:37 +0200

    added link back to index to the logo. Small css fixes.

--- header.php
+++ header.php
@@ -8,7 +8,9 @@
 	</head>
 <body>
     <div id="header">
-		<p>DogeBook</p>
+        <a href="index.php">
+            <p>DogeBook</p>
+        </a>
 		<div class="login">
 			<label for="username">Username:</label><input id="username" type="text" name="username" size="15" maxlength="30" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>">
 			<label for="password">Password:</label><input id="password" type="password" name="password" size="15" maxlength="15" value="" >
