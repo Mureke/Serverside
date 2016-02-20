d6f97dcf7dd3d47f9fe119cf41108b03da79f1d3
Markus Suomalainen <markus.suomalainen@kassa.fi>
2016-02-20 15:26:14 +0200

    Register page validation

--- register.php
+++ register.php
@@ -7,11 +7,47 @@
     $password = $_POST['password'];
     
     $user = new User;
-    $user->name = $name;
-    $user->email = $email;
-    $user->password = $password;
     
-    $user->create();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$errors = array(); // Initialize an error array.
+	// Check for a first name:
+	if (empty($_POST['username'])) {
+		$errors[] = 'You forgot to enter your username.';
+	} else {
+		$user->name = trim($_POST['username']);
+	}
+	// Check for an email address:
+	if (empty($_POST['email'])) {
+		$errors[] = 'You forgot to enter your email address.';
+	} else {
+		$user->email = trim($_POST['email']);
+	}
+	// Check for a password and match against the confirmed password:
+	if (!empty($_POST['password'])) {
+		if ($_POST['password'] != $_POST['password2']) {
+			$errors[] = 'Your two password did not match.';
+		} else {
+			$user->password = trim($_POST['password']);
+		}
+	} else {
+		$errors[] = 'You forgot to enter your password.';
+	}
+	if (empty($errors)) { // If everything's OK.
+	// Inform that all values are OK
+	echo '<p class="accountCreated">New account created!';
+         $user->create();
+	
+	} else { // Report the errors.
+		//header ("location: register-page.php"); 
+		echo '<h2>Error!</h2>
+		<p class="error">The following error(s) occurred:<br>';
+		foreach ($errors as $msg) { // Print each error.
+			echo " - $msg<br>\n";
+                }
+		}// End of if (empty($errors)) IF.
+} // End of the main Submit conditional.
+    
+   
     }
 ?>
 
@@ -21,7 +57,7 @@
 				<p class="register-input-text"><label class="label" for="email">Email:</label><input id="email" type="text" name="email" size="30" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
 				<p class="register-input-text"><label class="label" for="password">Password:</label><input id="password" type="password" name="password" size="30" maxlength="15" value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>" ></p>
 				<p class="register-input-text"><label class="label" for="password2">Confirm Password:</label><input id="password2" type="password" name="password2" size="30" maxlength="15" value="<?php if (isset($_POST['password2'])) echo $_POST['password2']; ?>" ></p>
-				<p class="register-input-text"><input id="submit" type="submit" name="submit" value="Register"></p>
+                                <p class="register-input-text"><button id="submit" type="submit" name="submit" value="Register">Register</button></p>
 		</form>
 	</div>
  <?php include('footer.php'); ?>
