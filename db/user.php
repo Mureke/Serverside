eff2c9d1d2dcc159014a155f3547646813002b46
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-02-20 14:01:36 +0200

    Register stuff. still in progress

---
+++ db/user.php
@@ -0,0 +1,21 @@
+<?php
+    require('connect.php');
+
+class User{
+    public $name;
+    public $email;
+    public $password;
+    // INSERT INTO `users`( `name`, `email`, `password`, `role`, `avatar`) VALUES ('admin','lol@lol.fi','admin','admin',3)
+        
+    public function create(){
+        $db = new Db();
+        $avatar = rand(2, 20);
+        $db -> query("INSERT INTO `users`( `name`, `email`, `password`, `role`, `avatar`) VALUES (" . $this->name . "," . $this->email . "," . $this->password . ",'User'," . $avatar . ");" );
+        echo $avatar;
+        $this->result = $db;
+       }
+      
+    }
+  
+
+
--- register.php
+++ register.php
@@ -1,11 +1,31 @@
+<?php require('db/user.php'); ?>
 <?php include('header.php');?>
+
 <div id="register-doge">
 		<form action="register.php" method="post">
-				<p><label class="label" for="username">Username:</label><input id="username" type="text" name="username" size="30" maxlength="30" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>"></p>
-				<p><label class="label" for="email">Email:</label><input id="email" type="text" name="email" size="30" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
-				<p><label class="label" for="password">Password:</label><input id="password" type="password" name="password" size="30" maxlength="15" value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>" ></p>
-				<p><label class="label" for="password2">Confirm Password:</label><input id="password2" type="password" name="password2" size="30" maxlength="15" value="<?php if (isset($_POST['password2'])) echo $_POST['password2']; ?>" ></p>
-				<p><input id="submit" type="submit" name="submit" value="Register"></p>
+				<p class="register-input-text"><label class="label" for="username">Username:</label><input id="username" type="text" name="username" size="30" maxlength="30" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>"></p>
+				<p class="register-input-text"><label class="label" for="email">Email:</label><input id="email" type="text" name="email" size="30" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
+				<p class="register-input-text"><label class="label" for="password">Password:</label><input id="password" type="password" name="password" size="30" maxlength="15" value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>" ></p>
+				<p class="register-input-text"><label class="label" for="password2">Confirm Password:</label><input id="password2" type="password" name="password2" size="30" maxlength="15" value="<?php if (isset($_POST['password2'])) echo $_POST['password2']; ?>" ></p>
+				<p class="register-input-text"><input id="submit" type="submit" name="submit" value="Register"></p>
 		</form>
 	</div>
+
+<?php
+if(!empty($_POST)){
+    $name = $_POST['username'];
+    $email= $_POST['email'];
+    $password = $_POST['password'];
+    
+    $user = new User;
+    $user->name = $name;
+    $user->email = $email;
+    $user->password = $password;
+    
+    $user->create();
+    }
+    else{
+        echo "vittu";
+    }
+ ?>
  <?php include('footer.php'); ?>
